52b0cf4d0d14b57c2915962638d92ab8a870667d
Matteo <guzzo.matteo@gmail.com>
2016-09-24 19:53:05 +0200

    Started implementation of eq. 6

--- sandbox/from_paper.py
+++ sandbox/from_paper.py
@@ -29,8 +29,21 @@ f_s = 1E6*1.0
 # Sampling period
 T_s = 1/f_s
 # plt.plot(ch0.imag)
+# Doppler frequency range
+f_dop = np.arange(-120,120,step=1)
+# time delay range
+R = 500
+l = np.arange(R)
 
-corr = signal.fftconvolve(ch0,ch1)
+corr = np.zeros((len(ch1)-R,len(f_dop)))
+
+for i in range(R,len(ch1)):
+    for j in range(len(f_dop)):
+        # shift_ch1[i] = ch1[i-R]
+        corr[i,j] = signal.fftconvolve(ch0[i],ch1[i-R]*np.exp(-1j*2*np.pi*f_dop[j]/(len(ch1)-R)))
+# shift_ch1[i:i+1] = ch1[i-R,i-R+1]
+
+# corr = signal.fftconvolve(ch0,ch1)
 
 print(corr[0])
 plt.plot(corr.real)

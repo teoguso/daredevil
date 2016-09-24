6d047a5e7b6438a6326f6dcc9a4ea79146d6821e
Matteo <guzzo.matteo@gmail.com>
2016-09-24 17:40:06 +0200

    First try reading the data.

--- sandbox/lms.py
+++ sandbox/lms.py
@@ -1,2 +1,12 @@
+from __future__ import print_function
 import numpy as np
 import adaptfilt
+from scipy import fromfile
+import matplotlib.pyplot as plt
+
+path = "../example_data/ch0.cfile"
+ch0 = fromfile(path)
+
+print(ch0.shape)
+plt.plot(ch0)
+plt.show()

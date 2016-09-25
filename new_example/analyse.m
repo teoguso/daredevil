b887891020f7b3c4b227fc183882e8d33ddebe87
Your Name <you@example.com>
2016-09-25 09:07:55 +0000

    x

--- new_example/analyse.m
+++ new_example/analyse.m
@@ -14,8 +14,32 @@ maxdelay = maxrange / c;
 fs = 200e3;
 len = min([length(ch0), length(ch1)]);
 
-maxshift = maxdelay * fs;
-maxtrans = maxdoppler / (fs / len);
+maxshift = round(maxdelay * fs);
+maxtrans = round(maxdoppler / (fs / len));
 
-%CH0=fft(ch0);
-%CH1=fft(ch1);
+CH0=fft(ch0);
+CH1=fft(ch1);
+
+out = zeros(2 * maxtrans + 1, 2 * maxshift + 1);
+
+for i=-maxtrans:maxtrans,
+  ch1s = CH1;
+  if (i<0)
+    ch0s = [CH0(-i+1:end).'; zeros(-i, 1)];
+  else
+    ch0s = CH0.';
+  end
+
+  if (i>0)
+    ch1s = [CH1(i+1:end).'; zeros(i, 1)];
+  else
+    ch1s = CH1.';
+  end
+
+  CORR = ch0s .* conj(ch1s);
+  corr = ifft(CORR);
+
+  out(i + maxtrans + 1, :) = [corr(end-maxshift : end)', corr(1:maxshift)'];
+  imagesc(abs(out));
+  pause(0.001);
+end

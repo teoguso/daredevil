a1c98a5c46f736438a443a120debfaf156f4a391
Your Name <you@example.com>
2016-09-25 09:43:21 +0000

    x

--- new_example/analyse.m
+++ new_example/analyse.m
@@ -1,45 +1,36 @@
 addpath ~/daredevil/multi-rtl/examples/utils/
-ch0=read_complex_binary('ch0.cfile');
-ch1=read_complex_binary('ch1.cfile');
-
-
-c=3e8;
-f0 = 88.8e6;
-maxrange = 100e3;
-maxvel = 500/3.6;
-maxdoppler = (c+maxvel)/c*f0 - f0;
-
-maxdelay = maxrange / c;
-
-fs = 200e3;
-len = min([length(ch0), length(ch1)]);
-
-maxshift = round(maxdelay * fs);
-maxtrans = round(maxdoppler / (fs / len));
-
-CH0=fft(ch0);
-CH1=fft(ch1);
 
 out = zeros(2 * maxtrans + 1, 2 * maxshift + 1);
 
-for i=-maxtrans:maxtrans,
-  ch1s = CH1;
-  if (i<0)
-    ch0s = [CH0(-i+1:end).'; zeros(-i, 1)];
-  else
-    ch0s = CH0.';
+for iter=1:100,
+%  system('./mutlirtl_rx_to_cfile_2chan.py');
+  ch0=read_complex_binary('ch0.cfile');
+  ch1=read_complex_binary('ch1.cfile');
+  
+  
+  c=3e8;
+  f0 = 88.8e6;
+  maxrange = 100e3;
+  maxvel = 500/3.6;
+  maxdoppler = (c+maxvel)/c*f0 - f0;
+
+  maxdelay = maxrange / c;
+
+  fs = 200e3;
+
+  maxshift = round(maxdelay * fs);
+  maxtrans = round(maxdoppler / (fs / len));
+
+  CH0=fft(ch0);
+  CH1=fft(ch1);
+
+  for i=-maxtrans:maxtrans,
+
+    CORR = CH0 .* conj(shift(CH1, i));
+    corr = ifft(CORR);
+
+    out(i + maxtrans + 1, :) = [corr(end-maxshift : end), corr(1:maxshift)];
+    image(abs(out)/100);
+    pause(0.001);
   end
-
-  if (i>0)
-    ch1s = [CH1(i+1:end).'; zeros(i, 1)];
-  else
-    ch1s = CH1.';
-  end
-
-  CORR = ch0s .* conj(ch1s);
-  corr = ifft(CORR);
-
-  out(i + maxtrans + 1, :) = [corr(end-maxshift : end)', corr(1:maxshift)'];
-  imagesc(abs(out));
-  pause(0.001);
 end
